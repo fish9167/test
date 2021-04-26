75e2dce063217cf5aa63722dd016279cbdf96d52
fish9167 <fish9167@users.noreply.github.com>
2021-04-26 11:47:37 +0000

    Apply clang-format

--- wahah.cpp
+++ wahah.cpp
@@ -2,48 +2,39 @@
 # 20210426
 
 #include "audio/include/AudioEngine.h"
-      #include <condition_variable>     
-       #include <condition_variable>     
-  #include <condition_variable>     
-                                    #include <condition_variable>
-
-
-
-
-                                    #include "audio/include/AudioEngine.h"
 #include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+
+#include "audio/include/AudioEngine.h"
 #include <condition_variable>
 
 #include <condition_variable>
 # 20210426
 
 #include "audio/include/AudioEngine.h"
-      #include <condition_variable>     
-#include <condition_variable>     
-  #include <condition_variable>     
-                                    #include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
 
 # 20210426
 
 #include "audio/include/AudioEngine.h"
-      #include <condition_variable>     
-#include <condition_variable>     
-  #include <condition_variable>     
-                                    #include <condition_variable>
-
-
-
-
-                                    #include "audio/include/AudioEngine.h"
 #include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+
+#include "audio/include/AudioEngine.h"
 #include <condition_variable>
 
 #include <condition_variable>
 # 20210426
 
 #include "audio/include/AudioEngine.h"
-      #include <condition_variable>     
-#include <condition_variable>     
-  #include <condition_variable>     
-                                    #include <condition_variable>
-
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
