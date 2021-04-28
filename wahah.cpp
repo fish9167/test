47fe9ea352299dca76e014cc0919a78cc78e96ae
fish9167 <fish9167@users.noreply.github.com>
2021-04-28 09:28:21 +0000

    Apply clang-format

--- wahah.cpp
+++ wahah.cpp
@@ -1,31 +1,19 @@
 
-          #@20210428
-              #@20210428
-          #@20210428
-6666666666666
-                    
-#include "audio/include/AudioEngine.h"
-#include <condition_variable>
-                    #include <condition_variable>
-                    #include <condition_variable>
-                    #include <condition_variable>           
-#include "audio/include/AudioEngine.h"
-#include <condition_variable>
-                    #include <condition_variable>          
-#include "audio/include/AudioEngine.h"
-#include <condition_variable>
-                    #include <condition_variable>
-                    #include <condition_variable>
-                    #include <condition_variable>
-                    #include <condition_variable>
-                    #include <condition_variable>   
+#@ 20210428 #@ 20210428 #@ 20210428 6666666666666
 
-#@ 20210428 #@ 20210428 #@ 20210428
-
-
-          
 #include "audio/include/AudioEngine.h"
 #include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+#include <condition_variable>
+
+    #@ 20210428 #@ 20210428 #@ 20210428
+
+#include "audio/include/AudioEngine.h"
 #include <condition_variable>
 # 2222222
     #@ 20210428
