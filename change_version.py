90d530185411775508c8f1fbc095da49977b71f6
fish9167 <674821954@qq.com>
2021-05-26 09:55:41 +0800

    Update change_version.py

--- change_version.py
+++ change_version.py
@@ -9,10 +9,13 @@ version=sys.argv[2]
 
 print("change version: %s, file: %s" %(version, file_path))
 
+print("json load")
 with open(file_path, "r",encoding='utf-8') as f:
 	data = json.load(f)
 
+print("change version")
 data["version"] = version
 
+print("save file")
 with open(file_path, "w") as f:
 	json.dump(data, f, indent=4, ensure_ascii=False)
